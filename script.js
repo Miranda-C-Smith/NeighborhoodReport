7dfea53f85a48e44ac53b42a8a2c608a3b6560af
Miranda Smith <msmit213@odu.edu>
2018-11-10 23:15:44 -0500

    create buttons

---
+++ script.js
@@ -0,0 +1,22 @@
+function sendMessage(){
+	var phoneNumber = "17574471488@tmomail.net";
+	
+	
+	
+}
+
+function firearmButtonClick(){
+	var x = document.getElementById("mainPage");
+	x.style.display = "none";
+	
+	var y = document.getElementById("firearmSubpage");
+	y.style.display = "block";
+}
+
+function cyberButtonClick() {
+	var x = document.getElementById("mainPage");
+	x.style.display = "none";
+	
+	var y = document.getElementById("cyberSubpage");
+	y.style.display = "block";
+}
